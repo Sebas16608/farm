f496a9a96e0d90b50d1f343eb6d77a01c2124a8f
Sebas16608 <asebasrr444@gmail.com>
2025-10-23 22:33:16 -0600

    Instalando la app de Cerdas y  configurando una base de datos de postgresql

--- registro/settings.py
+++ registro/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cerdas',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -74,8 +76,11 @@ WSGI_APPLICATION = 'registro.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'farm',
+        'USER': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
