ffac06f92938e324b0dc9234b0abc2b26ec22b83
Sebas16608 <asebasrr444@gmail.com>
2025-10-24 23:52:36 -0600

    definiendo el metodo DELETE de la API del control del celo

--- cerdas/views.py
+++ cerdas/views.py
@@ -82,3 +82,13 @@ class CeloView(APIView):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, pk):
+        try:
+            celo = ControlCelo.objects.get(pk=pk)
+        except ControlCelo.DoesNotExist:
+            return Response(notexist(), status=status.HTTP_404_NOT_FOUND)
+        
+        celo.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
