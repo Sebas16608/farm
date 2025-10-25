d539d55095db7145f9acd82845ac48aac627fd4f
Sebas16608 <asebasrr444@gmail.com>
2025-10-24 22:13:19 -0600

    Definiendo el metodo POST de la API para la app de cerdas

--- cerdas/views.py
+++ cerdas/views.py
@@ -21,3 +21,12 @@ class CerdaView(APIView):
             cerda = Cerda.objects.all()
             serializer = CerdaSerializer(cerda, many=True)
             return Response(serializer.data, status=status.HTTP_200_OK)
+        
+    def post(self, request):
+        serializer = CerdaSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    
