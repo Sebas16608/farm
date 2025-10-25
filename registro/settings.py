9e36fa0cd541002e2136ef721826735029290d19
Sebas16608 <asebasrr444@gmail.com>
2025-10-24 23:58:10 -0600

    Instalando jazzmin para que el admin sea mas bonito

--- registro/settings.py
+++ registro/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'cerdas',
-    'rest_framework'
+    'rest_framework',
+    'jazzmin'
 ]
 
 MIDDLEWARE = [
