71cf846ff35866a4597b243b693114e2664960f5
Sebas16608 <asebasrr444@gmail.com>
2025-12-26 00:23:25 -0600

    Arreglando un error de tipografia

--- cerdas/urls.py
+++ cerdas/urls.py
@@ -4,6 +4,6 @@ from django.urls import path
 urlpatterns = [
     path("cerda/", CerdaView.as_view(), name="cerda-list"),
     path("cerda/<int:pk>/", CerdaView.as_view(), name="cerdas-detail"),
-    path("contro-celo/", ControlCeloView.as_view(), name="celo-list"),
+    path("control-celo/", ControlCeloView.as_view(), name="celo-list"),
     path("control-celo/<int:pk>/", ControlCeloView.as_view(), name="celo-detail"),
 ]
