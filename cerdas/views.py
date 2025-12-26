53945f445eaf52f1cfa837ae55f4cfd662a36dc6
Sebas16608 <asebasrr444@gmail.com>
2025-12-26 00:16:58 -0600

    Definiendo las views que son unas APIs con la generic creada

--- cerdas/views.py
+++ cerdas/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from .models import Cerda, ControlCelo
+from API import SuperAPIView
+from .serliazers import CerdaSerializer, ControlCeloSerializer
 # Create your views here.
+
+class CerdaView(SuperAPIView):
+    model = Cerda
+    serializer = CerdaSerializer
+
+class ControlCeloView(SuperAPIView):
+    model = ControlCelo
+    serializer = ControlCeloSerializer
