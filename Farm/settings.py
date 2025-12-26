777376adea036a743ffbcee195fcbdb8494c4453
Sebas16608 <asebasrr444@gmail.com>
2025-12-26 01:05:11 -0600

    fix the host

--- Farm/settings.py
+++ Farm/settings.py
@@ -21,7 +21,7 @@ load_dotenv(BASE_DIR / ".env")
 SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = os.getenv("DEBUG") == "True"
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
+ALLOWED_HOSTS = ["https://granja-la-cruz-backend.onrender.com/"]
 
 # ------------------------------------------------------------------
 # APPLICATIONS
