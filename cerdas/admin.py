11ac4a03859430ab60c07af1bbd9bb961b7135a9
Sebas16608 <asebasrr444@gmail.com>
2025-12-26 00:10:31 -0600

    definiendo el admin

--- cerdas/admin.py
+++ cerdas/admin.py
@@ -1,3 +1,40 @@
+# admin.py
 from django.contrib import admin
+from .models import Cerda, ControlCelo
 
-# Register your models here.
+
+@admin.register(Cerda)
+class CerdaAdmin(admin.ModelAdmin):
+    list_display = (
+        'codigo',
+        'estado',
+        'activa',
+        'fecha_nacimiento',
+        'fecha_creacion',
+    )
+    list_filter = ('estado', 'activa')
+    search_fields = ('codigo',)
+    ordering = ('codigo',)
+    readonly_fields = ('fecha_creacion', 'fecha_actualizacion')
+
+
+@admin.register(ControlCelo)
+class ControlCeloAdmin(admin.ModelAdmin):
+    list_display = (
+        'cerda',
+        'fecha_celo',
+        'en_celo',
+        'fecha_servicio_recomendada',
+        'fecha_proximo_celo',
+    )
+    list_filter = ('en_celo', 'fecha_celo')
+    search_fields = ('cerda__codigo',)
+    ordering = ('-fecha_celo',)
+    readonly_fields = (
+        'fecha_servicio_recomendada',
+        'fecha_proximo_celo',
+        'fecha_creacion',
+        'fecha_actualizacion',
+    )
+
+    autocomplete_fields = ('cerda',)
