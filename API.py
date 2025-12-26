5a2f8ed753d199a1c3532bce6d6988ffd3d349c0
Sebas16608 <asebasrr444@gmail.com>
2025-12-26 00:13:38 -0600

    Creando una generic para facilitar el uso de la API

---
+++ API.py
@@ -0,0 +1,70 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+
+def notexist():
+    return {"error": "Los datos no fueron encontrados"}
+
+class SuperAPIView(APIView):
+    model = None
+    serializer = None
+
+    # def get_permissions(self):
+    #     if self.request.method in ['POST', 'PUT', 'PATCH', 'DELETE']:
+    #         return [IsAuthenticated()]
+    #     return []
+    
+    def get(self, request, pk=None):
+        if pk:
+            try:
+                obj = self.model.objects.get(pk=pk)
+                serializer = self.serializer(obj)
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            except self.model.DoesNotExist:
+                return Response(notexist(), status=status.HTTP_404_NOT_FOUND)
+        else:
+            obj = self.model.objects.all()
+            serializer = self.serializer(obj, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def post(self, request):
+        serializer = self.serializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk):
+        try:
+            obj = self.model.objects.get(pk=pk)
+        except self.model.DoesNotExist:
+            return Response(notexist(), status=status.HTTP_404_NOT_FOUND)
+        
+        serializer = self.serializer(obj, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def patch(self, request, pk):
+        try:
+            obj = self.model.objects.get(pk=pk)
+        except self.model.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        serializer = self.serializer(obj, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        try:
+            obj = self.model.objects.get(pk=pk)
+        except self.model.DoesNotExist:
+            return Response(notexist(), status=status.HTTP_404_NOT_FOUND)
+        
+        obj.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
